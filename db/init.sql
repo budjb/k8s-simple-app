5e33b54ed9e58506cf757ebfe736219ef9fdc0e9
Bud Byrd <bud.byrd@gmail.com>
2022-12-31 11:42:17 -0600

    Add DB init script

---
+++ db/init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE pets (
+    name VARCHAR(256) PRIMARY KEY,
+    type VARCHAR(32)
+);
+
+INSERT INTO pets (name, type) VALUES ('Biscuit', 'DOG');
+INSERT INTO pets (name, type) VALUES ('Spike', 'DOG');
+INSERT INTO pets (name, type) VALUES ('Tryxie', 'CAT');
