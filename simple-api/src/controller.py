2cab44411cca55ecf89d3606a08ee0aef5e38e20
Bud Byrd <bud.byrd@gmail.com>
2023-01-01 14:42:27 -0600

    Add some docs to python project.

--- simple-api/src/controller.py
+++ simple-api/src/controller.py
@@ -9,6 +9,12 @@ CORS(app, expose_headers=["X-Source-Host"])
 
 @app.route("/", methods=["GET"])
 def list_pets():
+    """
+    API endpoint that returns a complete list of pets in the database.
+
+    The response will contain the X-Source-Host header whose value is the
+    hostname of the server running the application.
+    """
     res = make_response(jsonify(pet_dao.list_pets()))
     res.headers["X-Source-Host"] = platform.node()
     return res
--- simple-api/src/pet_dao.py
+++ simple-api/src/pet_dao.py
@@ -3,14 +3,21 @@ from contextlib import contextmanager
 
 
 def list_pets():
+    """
+    Performs a database query to collect all pets and returns a list of
+    pets as dictionaries.
+    """
     with _get_connection() as connection:
         results = connection.execute("SELECT * FROM pets").fetchall()
-
         return [_marshall_record(record) for record in results]
 
 
 @contextmanager
 def _get_connection():
+    """
+    Yields a database connection object to the caller. When used with "with",
+    the database connection will automatically be cleaned up.
+    """
     with psycopg.connect(
         "postgresql://petstore:mypass@localhost:5433/petstore"
     ) as connection:
@@ -18,4 +25,9 @@ def _get_connection():
 
 
 def _marshall_record(record):
+    """
+    Given a database row tuple, converts the database response to a dictionary
+    with appropriately named keys. This method assumes column order as returned
+    by "SELECT * FROM pets".
+    """
     return {"name": record[0], "type": record[1]}
